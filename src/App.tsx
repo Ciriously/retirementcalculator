473755df5651549e714002d091d360da67354606
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2023-11-22 00:25:11 +0530

    added gradient to look cool

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Calculator from "./features/calculator/Calculator";
 import "./App.css";
 import RetirementChart from "./features/RetirementChart";
+import Banner from "./Banner";
 
 function App() {
   return (
     <>
+      <Banner />
       <Calculator />
+      {/* Use the proper component name */}
       {/* <RetirementChart /> */}
-      {/* Work on adding charts */}
     </>
   );
 }
---
+++ src/Banner.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="flex justify-center items-center bg-white-500 py-4">
+      <div className="text-center">
+        <h1 className="text-5xl  font-inter-bold mb-4">
+          <span
+            className="bg-gradient-to-r text-transparent bg-clip-text"
+            style={{
+              backgroundImage:
+                "linear-gradient(to right, #978AFC, #C387FF, #FF47AA, #FF51CE)",
+            }}
+          >
+            Retirement Calculator
+          </span>
+        </h1>
+        <p className="text-lg font-medium text-white font-inter font-sansserif">
+          "Calculate Your Future Financial Freedom Now!"
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
